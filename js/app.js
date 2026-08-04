197c148d9664ade278a8de08eca4602320ccc99f
Elena Correa <58674385+hello-elena-correa@users.noreply.github.com>
2026-08-03 19:45:31 -0500

    loadWords function was updated

--- js/app.js
+++ js/app.js
@@ -14,7 +14,13 @@ const levelElement = document.getElementById("level");
 
 async function loadWords() {
   const response = await fetch(API_URL);
-  words = await response.json();
+
+  console.log(response.status);
+  console.log(response.redirected);
+  console.log(response.url);
+
+  const text = await response.text();
+  console.log(text.substring(0, 300));
 }
 
 function getWeightedRandom(words) {
